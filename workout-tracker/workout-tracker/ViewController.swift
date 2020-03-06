80f0ee73226035cd102b942c2f5a7089fbde9e8d
Fragoso <hfragoso@MXN1800100-2.local>
2020-03-06 00:18:41 -0600

    initial commit

---
+++ workout-tracker/workout-tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workout-tracker
+//
+//  Created by Fragoso, Hector on 3/5/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
